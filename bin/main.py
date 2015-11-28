a6f0d6f63c42793490543e7c2546f1eaaa9c8ce0
Alex Jordan <alex@strugee.net>
2015-11-27 23:26:31 -0800

    License as AGPL3+

--- bin/main.py
+++ bin/main.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 
+# Copyright 2015 Alex Jordan <alex@strugee.net>.
+# 
+# This file is part of bindslash.
+# 
+# bindslash is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# bindslash is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public
+# License along with bindslash. If not, see
+# <http://www.gnu.org/licenses/>.
+
 import sys
 import os
 import importlib
