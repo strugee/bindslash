b3d319bab2a58d1a945d4a350ed37ec2287c23b2
Alex Jordan <alex@strugee.net>
2015-11-27 22:44:21 -0800

    Remove test plugins

--- plugins/TestPlugin.py
+++
@@ -1,5 +0,0 @@
-def importConfiguration(path):
-	print("Importing from system.")
-
-def exportConfiguration(path):
-	print("Exporting to system.")
--- plugins/TestPlugin2.py
+++
@@ -1,5 +0,0 @@
-def importConfiguration(path):
-	print("Importing from system. 2")
-
-def exportConfiguration(path):
-	print("Exporting to system. 2")
