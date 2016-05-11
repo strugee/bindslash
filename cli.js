73a5a32087d6ff301b931ebff6e8998e65841829
Alex Jordan <alex@strugee.net>
2016-05-11 15:03:03 -0700

    Write most of the CLI

--- cli.js
+++ cli.js
@@ -1,4 +1,44 @@
 var bindslash = require('./index.js');
 
-console.log('importing...');
-bindslash.exportConfiguration([{name: 'dpkg', path: 'Dpkg'}]);
+var fs = require('fs');
+
+var program = require('commander');
+var ini = require('ini');
+var action;
+
+program
+	.version('1.0.0')
+	.option('-c --config [path]', 'Configuration file to use [bindslash.ini]', 'bindslash.ini');
+
+program
+	.command('import')
+	.description('import from the system into the bindslash project directory')
+	.action(function() {
+		action = 'import';
+	});
+
+program
+	.command('export')
+	.description('export from the bindslash project directory into the system')
+	.action(function() {
+		action = 'export';
+	});
+
+program.parse(process.argv);
+
+console.log(typeof action);
+if (typeof action === 'undefined') {
+	process.exitCode = 1;
+	program.help();
+}
+
+var config = ini.parse(fs.readFileSync('./bindslash.ini', 'utf-8'));
+var plugins = [];
+
+for (var plugin in config.bindslash) {
+	var dir = config.bindslash[plugin].pluginDir || plugin;
+
+	plugins.push({name: plugin, path: dir});
+}
+
+//bindslash.importConfiguration(plugins);
